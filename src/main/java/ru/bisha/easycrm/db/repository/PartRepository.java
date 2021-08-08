0f3ca912b83ff7bb00a0b0670afe36588cc99f6a
Bisha13 <ershoww@gmail.com>
2021-08-08 17:08:12 +0300

    Deleting of stock working without anomalies

--- src/main/java/ru/bisha/easycrm/db/repository/PartRepository.java
+++ src/main/java/ru/bisha/easycrm/db/repository/PartRepository.java
@@ -4,6 +4,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import ru.bisha.easycrm.db.entity.Part;
 
+import java.util.List;
+
 @Repository
 public interface PartRepository extends JpaRepository<Part, Integer> {
+    List<Part> getAllByStockId(int id);
 }
--- src/main/java/ru/bisha/easycrm/service/implementation/StockServiceImp.java
+++ src/main/java/ru/bisha/easycrm/service/implementation/StockServiceImp.java
@@ -3,6 +3,7 @@ package ru.bisha.easycrm.service.implementation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import ru.bisha.easycrm.db.entity.Stock;
+import ru.bisha.easycrm.db.repository.PartRepository;
 import ru.bisha.easycrm.db.repository.StockRepository;
 import ru.bisha.easycrm.service.StockService;
 import java.util.List;
@@ -12,31 +13,46 @@ import java.util.Optional;
 public class StockServiceImp implements StockService {
 
     @Autowired
-    private StockRepository repository;
+    private StockRepository stockRepository;
+
+    @Autowired
+    private PartRepository partRepository;
 
     @Override
     public void deleteStockPart(int id) {
-        repository.deleteById(id);
+        var stock = stockRepository.findById(id);
+        if (stock.isEmpty()) {
+            return;
+        }
+        var parts = partRepository.getAllByStockId(id);
+        parts.forEach(p -> {
+            p.setIsStock(false);
+            p.setPurchasePrice(stock.get().getPurchase());
+            p.setName(stock.get().getName());
+            p.setPrice(stock.get().getPrice());
+            p.setStock(stockRepository.findById(0).orElseThrow());
+        });
+        stockRepository.deleteById(id);
     }
 
     @Override
     public List<Stock> getAllStockParts() {
 
-        return repository.findAll();
+        return stockRepository.findAll();
     }
 
     @Override
     public void updateStockPart(Stock part) {
-        repository.save(part);
+        stockRepository.save(part);
     }
 
     @Override
     public void save(Stock part) {
-        repository.save(part);
+        stockRepository.save(part);
     }
 
     @Override
     public Optional<Stock> getById(int id) {
-        return repository.findById(id);
+        return stockRepository.findById(id);
     }
 }
